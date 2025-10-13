762e46a2fda56d7f6a9a3053d1a950436244494b
PabloAlonsoRomero <pabloealonsor@gmail.com>
2025-10-13 15:51:21 -0600

    Funcionalidad de trails-list ts

--- src/app/pages/trails-list.component/trails-list.component.ts
+++ src/app/pages/trails-list.component/trails-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Trail } from '../../core/models/trail.model';
+import { TrailService } from '../../core/services/trail.service';
 
 @Component({
   selector: 'app-trails-list',
@@ -6,6 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './trails-list.component.css',
   standalone: false
 })
-export class TrailsListComponent {
+export class TrailsListComponent implements OnInit {
+  constructor(
+    private trailService : TrailService
+  ) {}
 
+  trails : Trail[] = [];
+
+  ngOnInit(): void {
+    this.trailService.getAllTrails().subscribe(trails => {
+      console.log(trails);
+      this.trails = trails;
+    })
+  }
 }
