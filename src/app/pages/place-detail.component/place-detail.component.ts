e5c6874094f04ec9632933373354344f8b402a02
PabloAlonsoRomero <pabloealonsor@gmail.com>
2025-10-13 14:56:17 -0600

    Traer placeById de la base de datos

--- src/app/pages/place-detail.component/place-detail.component.ts
+++ src/app/pages/place-detail.component/place-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaceService } from '../../core/services/place.service';
+import { Place } from '../../core/models/place.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-place-detail.component',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './place-detail.component.css',
   standalone: false
 })
-export class PlaceDetailComponent {
+export class PlaceDetailComponent implements OnInit {
+
+  place : Place | null = null;
+  
+  constructor(
+    private placeService : PlaceService,
+    private route : ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+      this.placeService.getPlaceById(id).subscribe(place => {
+        this.place = place;
+        console.log(place)
+      })
+  }
 
 }
