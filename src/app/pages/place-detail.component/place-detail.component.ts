41026d8212480ccf6491a489dcc4b9a76ce34fa4
PabloAlonsoRomero <pabloealonsor@gmail.com>
2025-10-13 15:30:22 -0600

    Visualizacion de place-detail

--- src/app/pages/place-detail.component/place-detail.component.ts
+++ src/app/pages/place-detail.component/place-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { PlaceService } from '../../core/services/place.service';
 import { Place } from '../../core/models/place.model';
 import { ActivatedRoute } from '@angular/router';
+import * as mapboxgl from 'mapbox-gl'
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-place-detail.component',
@@ -12,6 +14,10 @@ import { ActivatedRoute } from '@angular/router';
 export class PlaceDetailComponent implements OnInit {
 
   place : Place | null = null;
+
+  map! : mapboxgl.Map;
+  style = "mapbox://styles/mapbox/streets-v11";
+  markers : mapboxgl.Marker[] = [];
   
   constructor(
     private placeService : PlaceService,
@@ -19,11 +25,22 @@ export class PlaceDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-      const id = Number(this.route.snapshot.paramMap.get('id'));
-      this.placeService.getPlaceById(id).subscribe(place => {
-        this.place = place;
-        console.log(place)
-      })
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.placeService.getPlaceById(id).subscribe(place => {
+      this.place = place;
+      setTimeout(() => {
+        this.map = new mapboxgl.Map({
+          accessToken: environment.MAPBOX_TOKEN,
+          style: this.style,
+          container: 'mini-map',
+          center: [this.place!.longitude, this.place!.latitude],
+          zoom: 10
+        });
+        new mapboxgl.Marker()
+          .setLngLat([this.place!.longitude, this.place!.latitude])
+          .addTo(this.map);
+      }, 0);
+    });
   }
 
 }
